fc1769cd2a704fb187f51cafb20c2bdbbad3d3b3
Yuvraj Singh Rana <ranayuvraj99@gmail.com>
2024-07-23 15:02:54 +1000

    Added upload files functionality

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ import hashlib
 app = Flask(__name__)
 # Task 3a: Implement Image Upload Functionality
 # Configure the upload folder
-
-
+UPLOAD_FOLDER = '/usercode/ImageForgeApp/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
@@ -18,8 +18,17 @@ def index():
 # Task 4b: Handle Upload Request to Backend
 
 def upload():
-    # Task 3b: Implement Image Upload Functionality
+    if 'file1' in request.files and 'file2' in request.files:
+        g.file1 = request.files['file1']
+        g.file2 = request.files['file2']
 
+        if not os.path.exists(app.config['UPLOAD_FOLDER']):
+            os.makedirs(app.config['UPLOAD_FOLDER'])
+    
+        image1.save(os.path.join(app.config['UPLOAD_FOLDER'], g.file1.filename))
+        image2.save(os.path.join(app.config['UPLOAD_FOLDER'], g.file2.filename))
+    else:
+        return jsonify({'error': 'Upload two images for similarity calculation'}), 400
 
 
     # Task 7: Determine Forgery and Display Results
